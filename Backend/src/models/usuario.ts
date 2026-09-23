07dec08f85e505098ddbc9a2c13c6673242f0b24
Vitor Marins <vitorbarbosamarins@gmail.com>
2026-09-23 16:44:24 -0300

    fix(models): add 'type' import for Relation in Usuario model

--- Backend/src/models/usuario.ts
+++ Backend/src/models/usuario.ts
@@ -1,4 +1,4 @@
-import { Entity, Column, PrimaryGeneratedColumn, OneToMany } from "typeorm"
+import { Entity, Column, PrimaryGeneratedColumn, OneToMany, type Relation } from "typeorm"
 import { Arquivo } from "./arquivo.js"
 
 @Entity()
